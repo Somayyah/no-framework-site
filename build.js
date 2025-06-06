66258f3853f388cef7ce678fd75e17fd88908613
watari <watari@techwebunraveled.xyz>
2025-06-06 22:42:39 +0300

    I got the file names, time to inject them into pug

--- build.js
+++ build.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 
 const path = "src/html/content/";
+
 const dirs = ["not-yet-released", "posts", "portal", "projects", "side-ventures", "tags"];
+
 let	files = {
 	"not-yet-released" : [],
 	"posts" : [], 
@@ -11,15 +13,21 @@ let	files = {
 	"tags" : []
 };
 
+let completed = 0;
+
 for (let i = 0; i < dirs.length; i++) {
   const dir = dirs[i];
-  fs.readdir(path + dir, (err, files) => {
+  fs.readdir(path + dir, (err, files_list) => {
     if (err) throw err;
-    console.log("Files in %s : %s", dir, files);
+	for(let j = 0; j < files_list.length; j++)
+	{
+		if (files_list[j] != dir+".pug")
+			files[dir].push(files_list[j].replace(".pug", ".html"));
+	}
+	completed++;
+	console.log("%s : %s\n", dir , files[dir])
+	if (completed === dirs.length) {
+		// This way I won't need promises or awaits
+    }
   });
 }
-
-
-// Prepare some stuff before serving the site
-
-// Get content lists for each directory
